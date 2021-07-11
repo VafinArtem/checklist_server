c430bb028c44698c4bd422d69c9061197a229cb4
Vafin Artem <a.vafin85@gmail.com>
2021-07-11 23:41:24 +0500

    настройка роутов

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const checkRoutes = require("./routes/checklist");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/check", checkRoutes);
 
 app.use((req, res, next) => {
   res.sendFile(`./index.html`);
