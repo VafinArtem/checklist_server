601efc95afcf04dc771143722ad2a3f3ce43e98c
Vafin Artem <a.vafin85@gmail.com>
2021-07-14 00:47:01 +0500

    добавил пакет cors

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const sequelize = require("./utils/database");
 const checkRoutes = require("./routes/checklist");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/cards", checkRoutes);
 
 app.use((req, res, next) => {
-  // res.sendFile(`./index.html`);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
+  res.sendFile(`./index.html`);
 });
 
 const start = async () => {
