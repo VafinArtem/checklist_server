0edafafb801165cc438bf3898c17e6bba98d7d47
Vafin Artem <a.vafin85@gmail.com>
2021-07-14 01:03:27 +0500

    работа с cors

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const checkRoutes = require("./routes/checklist");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/cards", checkRoutes);
+app.use("/api/cards", cors(), checkRoutes);
 
 app.use((req, res, next) => {
   res.sendFile(`./index.html`);
