5cb89927533d65a02da3c5297048fa8ce74db03d
Vafin Artem <a.vafin85@gmail.com>
2021-07-17 15:22:27 +0500

    реализовал удаление и добавление новой карточки

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const cors = require("cors");
 const sequelize = require("./utils/database");
 const checkRoutes = require("./routes/checklist");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-console.log(process.env.PORT);
 
 const corsOptions = {
   origin: "http://127.0.0.1:3000",
@@ -18,9 +16,9 @@ const corsOptions = {
 app.use(express.json());
 app.use("/api/cards", cors(corsOptions), checkRoutes);
 
-app.use((req, res, next) => {
-  // res.sendFile(`./index.html`);
-});
+// app.use((req, res, next) => {
+// res.sendFile(`./index.html`);
+// });
 
 const start = async () => {
   try {
--- routes/checklist.js
+++ routes/checklist.js
@@ -23,6 +23,23 @@ router.post(`/complite/:id/:status`, async (req, res) => {
   }
 });
 
+router.post(`/add`, async (req, res) => {
+  try {
+    const card = await Card.create({
+      text: req.body.text,
+      isComplite: false,
+      project: "default",
+      cathegory: "default",
+    });
+    res.status(201).json({card});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: `Server error`
+    });
+  }
+});
+
 router.post(`/edit/:id/`, async (req, res) => {
   try {
     const card = await Card.findByPk(+req.params.id);
@@ -36,6 +53,19 @@ router.post(`/edit/:id/`, async (req, res) => {
 
 // router.put(`/:id`, (req, res) => {});
 
-// router.delete(`/:id`, (req, res) => {});
+router.delete(`/delete/:id`, async (req, res) => {
+  try {
+    const cards = await Card.findAll({
+      where: {
+        id: req.params.id
+      }
+    });
+    const card = cards[0];
+    await card.destroy();
+    res.status(200).json();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
